35f71e76e2569b1de3a80d4d51ae7b06df860264
sven-zo <svenzo@outlook.com>
2018-06-11 14:13:56 +0200

    fix: adds loadingscreen text

--- docs/js/main.js
+++ docs/js/main.js
@@ -295,6 +295,26 @@ class GameOverScreen {
         this._game.scene = new THREE.Scene();
         this._game.camera = new THREE.PerspectiveCamera(75, this._game.rendererWidth / this._game.rendererHeight, 0.1, 1000);
         this._game.scene.background = new THREE.Color('red');
+        this._gameOverText = document.getElementById('gameover-text');
+        const n = Math.floor(Math.random() * 3);
+        switch (n) {
+            case 0:
+                this._gameOverText.innerText = 'Game over';
+                break;
+            case 1:
+                this._gameOverText.innerText = 'Again!';
+                break;
+            case 2:
+                this._gameOverText.innerText = 'DEAD';
+                break;
+        }
+        this._restart();
+    }
+    _restart() {
+        setTimeout(() => {
+            this._gameOverText.innerText = '';
+            this._game.stage = new Level();
+        }, 500);
     }
     update() { }
 }
--- src/stages/gameOverScreen.ts
+++ src/stages/gameOverScreen.ts
@@ -1,5 +1,6 @@
 class GameOverScreen implements Stage {
   private _game: Game;
+  private _gameOverText: HTMLDivElement;
 
   constructor() {
     this._game = Game.getInstance();
@@ -11,6 +12,31 @@ class GameOverScreen implements Stage {
       1000
     );
     this._game.scene.background = new THREE.Color('red');
+
+    this._gameOverText = document.getElementById(
+      'gameover-text'
+    ) as HTMLDivElement;
+    const n = Math.floor(Math.random() * 3);
+    switch (n) {
+      case 0:
+        this._gameOverText.innerText = 'Game over';
+        break;
+      case 1:
+        this._gameOverText.innerText = 'Again!';
+        break;
+      case 2:
+        this._gameOverText.innerText = 'DEAD';
+        break;
+    }
+
+    this._restart();
+  }
+
+  private _restart() {
+    setTimeout(() => {
+      this._gameOverText.innerText = '';
+      this._game.stage = new Level();
+    }, 500);
   }
 
   update() {}
